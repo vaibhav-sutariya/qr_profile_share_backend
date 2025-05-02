a99fa5b112ad52929c2fb17e58dbb20aa146aece
vaibhav-sutariya <vsutariya428@gmail.com>
2025-05-02 17:56:19 +0530

    feat: add environment variables for dynamic link generation

--- src/data/services/dynamic-link.ts
+++ src/data/services/dynamic-link.ts
@@ -7,9 +7,9 @@ dotenv.config();
 export const generateDynamicLink = catchAsync(
 	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		const { id, name, email, photo, position, location } = req.body;
-		const longLink = `https://randomqr.page.link/?link=${encodeURIComponent(
-			`https://qr-profile-share-backend.onrender.com/api/v1/users/getOneUser/${id}`,
-		)}&apn=com.example.qr_profile_share&afl=${encodeURIComponent(
+		const longLink = `${process.env.DOMAIN_URI_PREFIX}/?link=${encodeURIComponent(
+			`${process.env.DEEP_LINK_DOMAIN}/api/v1/users/getOneUser/${id}`,
+		)}&apn=${process.env.PACKAGE_NAME}&afl=${encodeURIComponent(
 			"https://play.google.com/store/apps/details?id=com.quikhitch",
 		)}`;
 
