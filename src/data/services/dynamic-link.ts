e889f44e62d9c1ac2bd1eb3b28f71ec506109a87
vaibhav-sutariya <vsutariya428@gmail.com>
2025-05-02 16:12:42 +0530

    refactor: simplify dynamic link generation logic and remove axios dependency

--- src/data/services/dynamic-link.ts
+++ src/data/services/dynamic-link.ts
@@ -1,55 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import axios from "axios";
 import catchAsync from "../../utils/catch-async.js";
-import {
-	IDynamicLinkRequest,
-	IDynamicLinkResponse,
-} from "../dtos/dynamicLinkTypes";
 import dotenv from "dotenv";
+import { me } from "../controllers/user/user-controller.js";
 dotenv.config();
 
 export const generateDynamicLink = catchAsync(
-	async (
-		req: Request<
-			Record<string, unknown>,
-			Record<string, unknown>,
-			IDynamicLinkRequest
-		>,
-		res: Response<IDynamicLinkResponse>,
-		next: NextFunction,
-	): Promise<void> => {
+	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		const { id, name, email, photo, position, location } = req.body;
-
-		const dynamicLinkPayload = {
-			dynamicLinkInfo: {
-				domainUriPrefix: process.env.DOMAIN_URI_PREFIX,
-				link: `${process.env.DEEP_LINK_DOMAIN}/api/v1/users/getOneUser/${id}`,
-				androidInfo: {
-					androidPackageName: process.env.PACKAGE_NAME,
-				},
-				iosInfo: {
-					iosBundleId: process.env.PACKAGE_NAME,
-				},
-				socialMetaTagInfo: {
-					socialTitle: "QR Profile",
-					socialDescription: `${name} - ${position}`,
-					socialImageLink:
-						photo || `${process.env.DEEP_LINK_DOMAIN}/default.png`,
-				},
-			},
-			suffix: {
-				option: "SHORT",
-			},
-		};
-
-		const response = await axios.post(
-			`https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.API_KEY}`,
-			dynamicLinkPayload,
-		);
+		const longLink = `https://randomqr.page.link/?link=${encodeURIComponent(
+			`https://qr-profile-share-backend.onrender.com/api/v1/users/getOneUser/${id}`,
+		)}&apn=com.example.qr_profile_share&afl=${encodeURIComponent(
+			"https://play.google.com/store/apps/details?id=com.quikhitch",
+		)}`;
 
 		res.status(200).json({
 			status: "success",
-			link: response.data.shortLink,
+			message: "Dynamic link generated successfully",
+			link: longLink,
 		});
 	},
 );
